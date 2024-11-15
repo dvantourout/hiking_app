7d0d249546783f79e9c93669e6a8d7d1d3dc3208
Damien Vantourout <damien.vantourout@protonmail.ch>
2024-11-15 15:20:16 +0100

    Add Sentry

--- maps_backend/app.py
+++ maps_backend/app.py
@@ -4,6 +4,7 @@ load_dotenv()
 
 from typing import Annotated
 
+import sentry_sdk
 from fastapi import Depends, FastAPI, Query
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
@@ -11,12 +12,26 @@ from slowapi.middleware import SlowAPIMiddleware
 from slowapi.util import get_remote_address
 from sqlalchemy.orm import Session
 
-from maps_backend import schemas
+from maps_backend import config, schemas
 
 from .database import get_db
 from .repositories import PointRepository, PolygonRepository
 
-limiter = Limiter(key_func=get_remote_address, default_limits=["10/minute"])
+sentry_sdk.init(
+    dsn=config.SENTRY_DSN,
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    _experiments={
+        # Set continuous_profiling_auto_start to True
+        # to automatically start the profiler on when
+        # possible.
+        "continuous_profiling_auto_start": True,
+    },
+)
+
+
+limiter = Limiter(key_func=get_remote_address, default_limits=["120/minute"])
 
 app = FastAPI()
 app.state.limiter = limiter
@@ -26,7 +41,7 @@ app.add_middleware(SlowAPIMiddleware)
 
 @app.get("/")
 def root():
-    return {"hello": "world?"}
+    return {"hello": "world"}
 
 
 @app.get("/points", response_model=list[schemas.Point])
@@ -35,8 +50,6 @@ def get_points(
     limit: int | None = 1000,
     db: Session = Depends(get_db),
 ):
-    print(bbox)
-
     points_repository = PointRepository()
     points = points_repository.list(
         db,
@@ -72,3 +85,8 @@ def get_polygons(db: Session = Depends(get_db)):
         }
         for polygon in polygons
     ]
+
+
+@app.get("/sentry-debug")
+async def trigger_error():
+    division_by_zero = 1 / 0
--- maps_backend/config.py
+++ maps_backend/config.py
@@ -5,3 +5,5 @@ DB_PASSWORD = getenv("DB_PASSWORD")
 DB_HOST = getenv("DB_HOST")
 DB_PORT = getenv("DB_PORT")
 DB_NAME = getenv("DB_NAME")
+
+SENTRY_DSN = getenv("SENTRY_DSN")
